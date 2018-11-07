462303e1325be777cad9d87b6a14b1a44e1414d7
pradeeprao22 <pradeepraorayapudi@gmail.com>
2018-11-07 12:16:16 +0530

    remember me function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-    
     def show
         @user = User.find(params[:id])
     end
@@ -10,17 +9,12 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
-
         if @user.save
-
             log_in @user
-            flash[:success] = "Welcome to the Sample App!"
+            flash[:success] = "Welcome to the Product Listing APP!"
             redirect_to @user
-
         else
-
             render 'new'
-
         end
     end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_05_131730) do
+ActiveRecord::Schema.define(version: 2018_11_07_021820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2018_11_05_131730) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
