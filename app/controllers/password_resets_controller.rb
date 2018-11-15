c29de562eb8ebaab77d49e60f66f1c2476c65588
pradeeprao22 <pradeepraorayapudi@gmail.com>
2018-11-15 19:59:54 +0530

    update on user activation email

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -17,7 +17,4 @@ class PasswordResetsController < ApplicationController
 
   def edit
   end
-
-  def update
-  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,12 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+
+  host = 'example' # Don't use this literally; use your local dev host instead
+  #for developing in localhost
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
